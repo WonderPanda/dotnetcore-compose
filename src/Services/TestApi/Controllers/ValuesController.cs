934aa141f7442e2ab1f4613b3b8a9444b28076ed
Unknown <Jesse.Carter@diply.com>
2017-11-13 18:08:41 -0500

    Initial commit

---
+++ src/Services/TestApi/Controllers/ValuesController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CommonLibrary;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TestApi.Controllers
+{
+   [Route("api/[controller]")]
+   public class ValuesController : Controller
+   {
+
+      private readonly MyCoolLib _myCoolLib;
+
+      public ValuesController(MyCoolLib myCoolLib)
+      {
+         _myCoolLib = myCoolLib;
+      }
+
+      // GET api/values
+      [HttpGet]
+      public async Task<IActionResult> Get()
+      {
+            await Task.Delay(1);
+            return Ok(_myCoolLib.DoSomething());
+      }
+
+      // GET api/values/5
+      [HttpGet("{id}")]
+      public string Get(int id)
+      {
+         return "value";
+      }
+
+      // POST api/values
+      [HttpPost]
+      public void Post([FromBody]string value)
+      {
+      }
+
+      // PUT api/values/5
+      [HttpPut("{id}")]
+      public void Put(int id, [FromBody]string value)
+      {
+      }
+
+      // DELETE api/values/5
+      [HttpDelete("{id}")]
+      public void Delete(int id)
+      {
+      }
+   }
+}
---
+++ src/libs/CommonLibrary/MyCoolLib.cs
@@ -0,0 +1,10 @@
+namespace CommonLibrary
+{
+    public class MyCoolLib
+    {
+        public string DoSomething()
+        {
+            return "Hello from a common service";
+        }   
+    }
+}
